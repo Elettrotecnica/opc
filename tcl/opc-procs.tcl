c2e839d45d13743006bc9843966329e1fba1abf7
Antonio Pisano <antonio@elettrotecnica.it>
2021-06-22 22:50:58 +0200

    Handle temporary loss of connection and configured nodes that are not found on the server

--- tcl/opc-procs.tcl
+++ tcl/opc-procs.tcl
@@ -129,48 +129,64 @@ ad_proc -private opc::monitor {
 	    ns_log warning "OPC Server '$url' - We do not use encryption."
 	}
 
-	# connect to server
-	opcua connect C $url
+	try {
+	    # connect to server
+	    opcua connect C $url
+	} on ok {d} {
 
-	ns_log warning "OPC Server '$url' - Creating subscription, interval='$interval'"
-	set sub [opcua subscription C new 1 $interval]
+	    ns_log warning "OPC Server '$url' - Creating subscription, interval='$interval'"
+	    set sub [opcua subscription C new 1 $interval]
 
-	# Every time one of the monitored variable changes, we send
-	# the updated value to a websocket subscription named after
-	# the server URL and the node.
-	# We also take note of the current value in an nsv.
-	proc ws_send {url node data} {
-	    if {[dict exists $data value]} {
-		set value [dict get $data value]
-	    } else {
-		set value $data
+	    # Every time one of the monitored variable changes, we send
+	    # the updated value to a websocket subscription named after
+	    # the server URL and the node.
+	    # We also take note of the current value in an nsv.
+	    proc ws_send {url node data} {
+		if {[dict exists $data value]} {
+		    set value [dict get $data value]
+		} else {
+		    set value $data
+		}
+		nsv_dict set ::opc::status $url $node $value
+		set json [::json::write object \
+			      url [::json::write string $url] \
+			      node [::json::write string $node] \
+			      value [::json::write string $value]]
+		nsv_dict set ::opc::status_json $url $node $json
+		# ns_log warning "Sending message on subscription 'opc-events':\n$json"
+		::ws::multicast opc-events [ns_connchan wsencode -opcode text $json]
 	    }
-	    nsv_dict set ::opc::status $url $node $value
-	    set json [::json::write object \
-			  url [::json::write string $url] \
-			  node [::json::write string $node] \
-			  value [::json::write string $value]]
-	    nsv_dict set ::opc::status_json $url $node $json
-	    # ns_log warning "Sending message on subscription 'opc-events':\n$json"
-	    ::ws::multicast opc-events [ns_connchan wsencode -opcode text $json]
+
+	    set i 0
+	    foreach node_id $monitored_nodes {
+		proc callback_${i} {data} [subst -nocommands {
+		    ws_send "${url}" "${node_id}" \$data
+		}]
+		try {
+		    set mon [opcua monitor C new $sub data callback_${i} $node_id]
+		} on ok {d} {
+		    ns_log warning "OPC Server '$url' - Monitoring node '$node_id', monitor=$mon"
+		    incr i
+		} on error {errMsg} {
+		    ns_log error "OPC Server '$url' - Monitoring node '$node_id' failed: $errMsg"
+		}
+	    }
+
+	    set to_monitor [llength $monitored_nodes]
+	    if {$i != $to_monitor} {
+		ns_log warning "OPC Server '$url' - Some nodes cannot be monitored. ($i/$to_monitor)"
+	    }
+
+	    opc::_check_connection \
+		-var disconnected \
+		-handle C \
+		-url $url
+
+	    vwait disconnected
+
+	} on error {errMsg} {
+	    ns_log warning "OPC Server '$url' - Connection failed: $errMsg"
 	}
-
-	set i 0
-	foreach node_id $monitored_nodes {
-	    proc callback_${i} {data} [subst -nocommands {
-		ws_send "${url}" "${node_id}" \$data
-	    }]
-	    set mon [opcua monitor C new $sub data callback_${i} $node_id]
-	    ns_log warning "OPC Server '$url' - Monitoring node '$node_id', monitor=$mon"
-	    incr i
-	}
-
-	opc::_check_connection \
-	    -var disconnected \
-	    -handle C \
-	    -url $url
-
-	vwait disconnected
 
 	ns_log warning "OPC Server '$url' - Reconnecting in 10s..."
 	ns_sleep 10
